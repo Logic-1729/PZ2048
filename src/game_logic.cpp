df8bd694aaaa7bf1d840633d048bfd3feee01ba6
PhantomPhoenix <logic_1729@sjtu.edu.cn>
2025-07-20 17:06:19 +0800

    fix: Adjust the format of ans document

--- src/game_logic.cpp
+++ src/game_logic.cpp
@@ -1,7 +1,9 @@
 #include "include/game_logic.h"
-
-#include <iomanip>
-#include <iostream>
+//WARNING: DO NOT DELETE THE THREE LINES BELOW!
+//WHEN YOU PUT YOUR CODE TO ACMOJ, YOU WOULD DELETE THE LINE 1 AND USE LINE 4-6 INSTEAD 
+//#include "game_logic.h"
+//#include "utils.h"
+//#include "utils.cpp"
 
 namespace PZ2048 {
 
@@ -15,246 +17,63 @@ namespace PZ2048 {
 
 // Don't try to use extern variables from user_logic.cpp. You're not supposed to communicate through this way.
 // After all, the game logic on the online judger won't contain extern variables same as yours.
-
-static int rows, cols, steps, score, prev_steps, prev_score, puzzle_target;
-static std::vector<std::vector<int>> board, prev_board;
-static bool prev_valid;
-
 // You are free to add helper functions.
 
 void Start(int row_num, int col_num, int target, uint game_seed) {
-  srand(game_seed); // Should be the PZ2048::srand(uint).
-  rows = row_num;
-  cols = col_num;
-  puzzle_target = target;
-
-  // You may want to add more lines.
-  steps = 0;
-  score = 0;
-  board.clear();
-  board.resize(rows, std::vector<int>(cols));
-  prev_board.resize(rows, std::vector<int>(cols));
-  prev_valid = false;
-
-  TryGenerateTile();
+    /** implement here. **/
 }
 
 std::pair<int, int> EndGame() {
-  /** implementation here **/
-  return {steps, score};
+    /** implementation here **/
 }
 
-int GetRows() { return rows; }
-
-int GetCols() { return cols; }
-
-bool TryRunW() {
-  static std::vector<int> arr;
-  bool valid = false;
-  for(int j = 0; j < cols; j++) {
-    arr.clear();
-    arr.resize(rows);
-    int t = 0;
-    for(int i = 0; i < rows; i++)
-      if(board[i][j] > 0) arr[t++] = board[i][j];
-    for(int i = 0; i + 1 < t; i++)
-      if(arr[i] == arr[i + 1]) {
-        arr[i] *= 2;
-        score += arr[i];
-        for(int k = i + 1; k < t - 1; k++)
-          arr[k] = arr[k + 1];
-        arr[--t] = 0;
-      }
-    for(int i = 0; i < rows; i++) {
-      if(arr[i] != board[i][j]) {
-        if(!valid) {
-          prev_board = board;
-          prev_score = score;
-          prev_steps = steps;
-          prev_valid = true;
-        }
-        valid = true;
-        board[i][j] = arr[i];
-      }
-    }
-  }
-  return valid;
+int GetRows() {
+    /** implementation here **/
 }
 
-bool TryRunS() {
-  static std::vector<int> arr;
-  bool valid = false;
-  for(int j = 0; j < cols; j++) {
-    arr.clear();
-    arr.resize(rows);
-    int t = rows - 1;
-    for(int i = rows - 1; i >= 0; i--)
-      if(board[i][j] > 0) arr[t--] = board[i][j];
-    for(int i = rows - 1; i - 1 > t; i--)
-      if(arr[i] == arr[i - 1]) {
-        arr[i] *= 2;
-        score += arr[i];
-        for(int k = i - 1; k > t + 1; k--)
-          arr[k] = arr[k - 1];
-        arr[++t] = 0;
-      }
-    for(int i = rows - 1; i >= 0; i--) {
-      if(arr[i] != board[i][j]) {
-        if(!valid) {
-          prev_board = board;
-          prev_score = score;
-          prev_steps = steps;
-          prev_valid = true;
-        }
-        valid = true;
-        board[i][j] = arr[i];
-      }
-    }
-  }
-  return valid;
-}
-
-bool TryRunA() {
-  static std::vector<int> arr;
-  bool valid = false;
-  for(int i = 0; i < rows; i++) {
-    arr.clear();
-    arr.resize(cols);
-    int t = 0;
-    for(int j = 0; j < cols; j++)
-      if(board[i][j] > 0) arr[t++] = board[i][j];
-    for(int j = 0; j + 1 < t; j++)
-      if(arr[j] == arr[j + 1]) {
-        arr[j] *= 2;
-        score += arr[j];
-        for(int k = j + 1; k < t - 1; k++)
-          arr[k] = arr[k + 1];
-        arr[--t] = 0;
-      }
-    for(int j = 0; j < cols; j++) {
-      if(arr[j] != board[i][j]) {
-        if(!valid) {
-          prev_board = board;
-          prev_score = score;
-          prev_steps = steps;
-          prev_valid = true;
-        }
-        valid = true;
-        board[i][j] = arr[j];
-      }
-    }
-  }
-  return valid;
-}
-
-bool TryRunD() {
-  static std::vector<int> arr;
-  bool valid = false;
-  for(int i = 0; i < rows; i++) {
-    arr.clear();
-    arr.resize(cols);
-    int t = cols - 1;
-    for(int j = cols - 1; j >= 0; j--)
-      if(board[i][j] > 0) arr[t--] = board[i][j];
-    for(int j = cols - 1; j - 1 > t; j--)
-      if(arr[j] == arr[j - 1]) {
-        arr[j] *= 2;
-        score += arr[j];
-        for(int k = j - 1; k > t + 1; k--)
-          arr[k] = arr[k - 1];
-        arr[++t] = 0;
-      }
-    for(int j = cols - 1; j >= 0; j--) {
-      if(arr[j] != board[i][j]) {
-        if(!valid) {
-          prev_board = board;
-          prev_score = score;
-          prev_steps = steps;
-          prev_valid = true;
-        }
-        valid = true;
-        board[i][j] = arr[j];
-      }
-    }
-  }
-  return valid;
+int GetCols() { 
+    /** implementation here **/
 }
 
 bool TryRun(char dir) {
-  /** implementation here **/
-  bool valid = false;
-  switch(dir) {
-  case 'w': valid = TryRunW(); break;
-  case 's': valid = TryRunS(); break;
-  case 'a': valid = TryRunA(); break;
-  case 'd': valid = TryRunD(); break;
-  default: break;
-  }
-  if(valid) {
-    steps++;
-    TryGenerateTile();
-  }
-  return valid;
+    /** implementation here **/
 }
 
 bool Undo() {
-  if(!prev_valid) return false;
-  board = prev_board;
-  score = prev_score;
-  steps = prev_steps;
-  prev_valid = false;
-  return true;
+    /** implement here. **/
 }
 
 void SetTile(int row_index, int col_index, int value) {
-  board[row_index][col_index] = value;
+    /** implement here. **/
 }
 
 int GetTile(int row_index, int col_index) {
-  return board[row_index][col_index];
+    /** implement here. **/
 }
 
-int Score() { return score; }
+int Score() { 
+    /** implement here. **/ 
+}
 
-int Steps() { return steps; }
+int Steps() { 
+    /** implement here. **/ 
+}
 
-int GetTarget() { return puzzle_target; }
-
+int GetTarget() { 
+    /** implement here. **/ 
+}
 
 bool HasReachedTarget() {
-  for(int i = 0; i < rows; i++)
-    for(int j = 0; j < cols; j++)
-      if(board[i][j] >= puzzle_target) {
-        return true;
-      }
-  return false;
+    /** implement here. **/
 }
 
 bool Stuck() {
-  /** implementation here **/
-  for(int i = 0; i < rows; i++)
-    for(int j = 0; j < cols; j++)
-      if(board[i][j] == 0)
-        return false;
-  for(int i = 0; i < rows - 1; i++)
-    for(int j = 0; j < cols; j++)
-      if(board[i][j] == board[i + 1][j])
-        return false;
-  for(int i = 0; i < rows; i++)
-    for(int j = 0; j < cols - 1; j++)
-      if(board[i][j] == board[i][j + 1])
-        return false;
-  return true;
+    /** implement here. **/
 }
 
 void PrintBoard() {
-  for(int i = 0; i < rows; i++) {
-    for(int j = 0; j < cols; j++) {
-      std::cout << std::left << std::setw(4) << board[i][j];
-      if(j != cols - 1) std::cout << ' ';
-    }
-    if(i != rows) std::cout << '\n';
-  }
+    /** implement here. **/
+    /** Hint: You MUST USE std:cout **/
 }
 
 }
